308f543d25befa38f467cd9d2cba6d61def368cd
Tom Moulard <tom.moulard@epita.fr>
2020-10-20 18:18:22 +0200

    htransformation_test: adding tests

--- htransformation_test.go
+++ htransformation_test.go
@@ -1,12 +1,111 @@
 package htransformation_test
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	plug "github.com/tommoulard/htransformation"
 )
 
-func TestDummy(t *testing.T) {
-	cfg := plug.CreateConfig()
-	t.Log(cfg)
+func assertHeader(t *testing.T, req *http.Request, key, expected string) {
+	t.Helper()
+
+	h := req.Header.Get(key)
+	if h != expected {
+		t.Errorf("invalid header value, got '%s', expect: '%s'", h, expected)
+	}
+}
+
+func TestOneHeader(t *testing.T) {
+	type fields struct {
+	}
+	type args struct {
+		rw  http.ResponseWriter
+		req *http.Request
+	}
+	tests := []struct {
+		name			string
+		transformations	plug.Transform
+		headers			map[string]string
+		want			map[string]string
+	}{
+		{
+			name: "no transformation",
+			transformations: plug.Transform{
+				Rename: "not-existing",
+			},
+			headers: map[string]string{
+				"Foo": "Bar",
+			},
+			want: map[string]string{
+				"Foo": "Bar",
+			},
+		},
+		{
+			name: "one transformation",
+			transformations: plug.Transform{
+				Rename: "Test",
+				With:   "Testing",
+				Type:   "",
+			},
+			headers: map[string]string{
+				"Foo": "Bar",
+				"Test": "Success",
+			},
+			want: map[string]string{
+				"Foo": "Bar",
+				"Testing": "Success",
+			},
+		},
+		{
+			name: "more transformation",
+			transformations: plug.Transform{
+				Rename: "Test*",
+				With:   "Testing",
+				Type:   "",
+			},
+			headers: map[string]string{
+				"Foo": "Bar",
+				"Test1": "Success",
+				"Test2": "Pass",
+			},
+			want: map[string]string{
+				"Foo": "Bar",
+				"Testing": "Pass",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cfg := plug.CreateConfig()
+			cfg.Transformations = []plug.Transform{tt.transformations}
+
+			ctx := context.Background()
+			next := http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {})
+
+			handler, err := plug.New(ctx, next, cfg, "demo-plugin")
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			recorder := httptest.NewRecorder()
+
+			req, err := http.NewRequestWithContext(ctx, http.MethodGet, "http://localhost", nil)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for hName, hVal := range tt.headers {
+				req.Header.Add(hName, hVal)
+			}
+
+			handler.ServeHTTP(recorder, req)
+
+			for hName, hVal := range tt.want {
+				assertHeader(t, req, hName, hVal)
+			}
+		})
+	}
 }
