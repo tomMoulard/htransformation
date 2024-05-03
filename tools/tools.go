f95cdcebdb8ae8884c50e18e9e24b576d5aa21c8
Tom Moulard <tom@moulard.org>
2024-05-03 15:00:06 +0200

    feat: add yaegi to tools

--- tools/tools.go
+++ tools/tools.go
@@ -11,5 +11,6 @@ package tools
 import (
 	_ "github.com/client9/misspell/cmd/misspell"
 	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
+	_ "github.com/traefik/yaegi/cmd/yaegi"
 	_ "golang.org/x/vuln/cmd/govulncheck"
 )
